dc9e1e304064115e1311f7e1c0ee8ad511869ade
D-Big-B <prjjwlbhatt@gmail.com>
2022-06-30 09:35:30 +0530

    Key Fixed

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/XhmchfEPUJ-VQrBo_T_Jqa8vJlttLi54",
-      accounts: [
-        "be8e041839c3a7514752e1a3735d6fd433dff62ece58f01b7e220d37f8063688",
-      ],
+      url: process.env.ALCHEMY_API,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
